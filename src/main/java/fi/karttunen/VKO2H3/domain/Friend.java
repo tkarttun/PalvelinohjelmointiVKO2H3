c7176afbec7d5a69c92f20e3e8f22213b5250e32
Toni Karttunen <toni.karttunen@gmail.com>
2021-09-05 16:21:32 +0300

    Toimiva versio, lomakkeen arvon tyhjennys puuttuu vielä.

--- src/main/java/fi/karttunen/VKO2H3/domain/Friend.java
+++ src/main/java/fi/karttunen/VKO2H3/domain/Friend.java
@@ -1,5 +1,28 @@
 package fi.karttunen.VKO2H3.domain;
 
 public class Friend {
+	private String name;
 
+	public Friend(String name) {
+		super();
+		this.name = name;
+	}
+
+	public Friend() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Friend [name=" + name + "]";
+	}
 }
--- src/main/java/fi/karttunen/VKO2H3/web/FriendController.java
+++ src/main/java/fi/karttunen/VKO2H3/web/FriendController.java
@@ -1,5 +1,32 @@
 package fi.karttunen.VKO2H3.web;
 
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fi.karttunen.VKO2H3.domain.Friend;
+
+@Controller
 public class FriendController {
 
+	ArrayList<Friend> friends = new ArrayList<>();
+	
+	@RequestMapping(value = "/index", method=RequestMethod.GET)
+	public String friendList(Model model) {
+		model.addAttribute("friend", new Friend());
+		return "index";
+	}
+	@RequestMapping(value = "/index", method = RequestMethod.POST)
+	public String friendListSubmit(@ModelAttribute Friend friend, Model model) {
+		if (friend != null) {
+			friends.add(friend);
+		}
+		model.addAttribute("friend", friend);
+		model.addAttribute("friends", friends);
+		return "result";
+	}	
 }
